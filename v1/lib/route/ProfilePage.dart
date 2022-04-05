1256d75bc65ddb0e13f73be20c1dddb7d4c193cf
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-04-04 21:26:57 -0700

    tile card and rounded edge

--- v1/lib/route/ProfilePage.dart
+++ v1/lib/route/ProfilePage.dart
@@ -19,10 +19,25 @@ class _MyProfilePageState extends State<MyProfilePage> {
             flex: 3,
             child: Column (
               children: <Widget>[
-                const Text(
-                    "Here should be the profile page"
-                ),
-                Image.network("https://cdn.jsdelivr.net/gh/Plan-At/static-image@main/2022/04/02/android-chrome-512x512.png") // SVG will not work here
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: <Widget>[
+                        Image.network("https://cdn.jsdelivr.net/gh/Plan-At/static-image@main/2022/04/02/android-chrome-512x512.png"), // SVG will not work here
+                        const Text(
+                          "Your Name",
+                          style: TextStyle(
+                              fontSize: 32
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
               ],
             ),
           ),
