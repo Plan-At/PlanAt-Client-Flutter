e725db7e586c9837299f32f738375c51c63f1116
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-04-08 12:51:09 -0700

    overview of the calendar

--- v1/lib/route/CalendarPage.dart
+++ v1/lib/route/CalendarPage.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class MyCalendarPage extends StatefulWidget {
@@ -10,6 +12,86 @@ class MyCalendarPage extends StatefulWidget {
 class _MyCalendarPageState extends State<MyCalendarPage> {
   @override
   Widget build(BuildContext context) {
-    return const Text("here should be the calendar page");
+    return Column(
+      children: [
+        Row(
+          children: const [
+            Text("Weekly"),
+            Spacer(
+              flex: 9,
+            ),
+            Text("Previous Week"),
+            Spacer(
+              flex: 1,
+            ),
+            Text("Next Week"),
+          ],
+        ),
+        const Divider(
+          height: 1,
+          thickness: 1,
+          indent: 0,
+          endIndent: 0,
+          color: Colors.black,
+        ),
+        Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  Text("here should be the calendar page")
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  Text("here should be the calendar page")
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  Text("here should be the calendar page")
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  Text("here should be the calendar page")
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  Text("here should be the calendar page"),
+                  Card(
+                    child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      onTap: () {
+                        debugPrint('Card tapped.');
+                      },
+                      child: const SizedBox(
+                        // width: 300,
+                        // height: 100,
+                        child: Text('A card that can be tapped'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
