c5527d7b0ae861f44579f41bf4e4012cef2a649d
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-04-08 18:28:24 -0700

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,8 +14,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "47cf223801af4c0a07379906b25b53c5",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4d85b2a855489893125def637b91dfeb",
-"/": "4d85b2a855489893125def637b91dfeb",
+"index.html": "9239c6ada593e8861b6e7654012bd735",
+"/": "9239c6ada593e8861b6e7654012bd735",
 "main.dart.js": "2a856c9b2394c67aafa111bd61f7f99f",
 "manifest.json": "9a2cb9da5118b3dd8a21604162ee6a52",
 "version.json": "92b5f87b1e9705808693fbf0571623e0"
